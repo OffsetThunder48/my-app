11f08414ac716983fdbe37d923e9eee750fc9530
lonewolf2k <lonewolf2k@protonmail.com>
2020-09-14 11:38:28 +0530

    Updates

---
+++ precache-manifest.8dcb88bb757313896c96e272a94ebbbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3aec8dd0545ff03a6a3531d8da62eb0",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "e27fc2e04831bf44d80d",
+    "url": "/my-app/static/css/main.4820205b.chunk.css"
+  },
+  {
+    "revision": "6eb64deb9ec83271757a",
+    "url": "/my-app/static/js/2.1d38a60f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.1d38a60f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e27fc2e04831bf44d80d",
+    "url": "/my-app/static/js/main.bef962a1.chunk.js"
+  },
+  {
+    "revision": "eecba5ef0aaf8dd269fe",
+    "url": "/my-app/static/js/runtime-main.7146fcda.js"
+  }
+]);
